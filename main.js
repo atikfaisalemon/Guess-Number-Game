1beb7aff761da0968bfc92526ed2f32954acde92
atikfaisalemon <atikfaisalemon@gmail.com>
2024-03-27 04:58:58 +0600

    random number works

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const userInput = document.getElementById("inputNum");
+const number = Math.trunc(Math.random() * 20);
+const checkBtn = document.getElementById("btn");
+console.log(number);
 
-setupCounter(document.querySelector('#counter'))
+document.getElementById("btn").addEventListener("click", displayNUm);
+
+function displayNUm() {
+  console.log(userInput.value);
+  colorChange();
+}
+
+function colorChange() {
+  if (number == userInput.value) {
+    document.getElementById("body").style.backgroundColor = "green";
+    document.getElementById("theNumber").innerHTML = userInput.value;
+  } else {
+    document.getElementById("body").style.backgroundColor = "red";
+    console.log("no red");
+  }
+}
