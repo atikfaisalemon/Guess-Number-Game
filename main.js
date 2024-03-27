a5a963889a0f679758fd554d6b07c32e6c368f4d
atikfaisalemon <atikfaisalemon@gmail.com>
2024-03-27 23:06:50 +0600

    last push

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 import "./style.css";
 
 const userInput = document.getElementById("inputNum");
-const number = Math.trunc(Math.random() * 10);
+let number = Math.trunc(Math.random() * 10);
 const checkBtn = document.getElementById("btn");
+
 console.log(number);
 
 document.getElementById("btn").addEventListener("click", displayNUm);
@@ -17,16 +18,35 @@ function colorChange() {
     document.getElementById("body").style.backgroundColor = "green";
     document.getElementById("theNumber").innerHTML = userInput.value;
     document.getElementById("status").innerHTML = "The number is currect...";
+    scoreCalculate();
   } else if (number > userInput.value) {
     document.getElementById("status").innerHTML = "Number is low...";
+    document.getElementById("status").style.color = "white";
   } else if (number < userInput.value) {
     document.getElementById("status").innerHTML = "Number is high...";
+    document.getElementById("status").style.color = "white";
+  } else if (number != userInput.value) {
+    document.getElementById("status").innerHTML = "Input Valid number";
+    document.getElementById("status").style.color = "red";
   } else {
-    ocument.getElementById("status").innerHTML = "Input Valid number";
-    document.getElementById("body").style.backgroundColor = "red";
-    console.log("no red");
   }
-  if (number != userInput.value) {
-    document.getElementById("body").style.backgroundColor = "gray";
+}
+
+document.getElementById("again").addEventListener("click", refresh);
+
+function refresh() {
+  // window.location.reload();
+  number = Math.trunc(Math.random() * 10);
+  document.getElementById("body").style.backgroundColor = "gray";
+  console.log("random", number);
+}
+
+function scoreCalculate() {
+  const scoreZero = document.getElementById("score").textContent;
+  let score = parseInt(scoreZero);
+
+  if (number == userInput.value) {
+    score++;
+    document.getElementById("score").innerHTML = score;
   }
 }
